421f09baa33764a87968e543c34053c7e6ccf38c
cpsola <cperez@deic.uab.cat>
2017-02-16 16:40:01 +0100

    Save utxo count and amount data into two separate json files

--- public/js/render-blockchain.js
+++ public/js/render-blockchain.js
@@ -5,6 +5,33 @@ var $ = require('jquery');
 var config = require('./config');
 var renderMagnifier = require('./render-magnifier');
 
+(function(console){
+
+console.save = function(data, filename){
+
+    if(!data) {
+        console.error('Console.save: No data')
+        return;
+    }
+
+    if(!filename) filename = 'console.json'
+
+    if(typeof data === "object"){
+        data = JSON.stringify(data, undefined, 4)
+    }
+
+    var blob = new Blob([data], {type: 'text/json'}),
+        e    = document.createEvent('MouseEvents'),
+        a    = document.createElement('a')
+
+    a.download = filename
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    a.dispatchEvent(e)
+ }
+})(console)
+
 module.exports = function renderBlockchain(id, loadFunction, meta, colorFunction) {
   var block = {};
   var max = 0;
@@ -30,7 +57,8 @@ module.exports = function renderBlockchain(id, loadFunction, meta, colorFunction
 
         block[height] = value;
       }
-
+	  console.save(block);
+	  
       // Render block data.
       var canvas = document.getElementById(id);
       var context = canvas.getContext('2d');
